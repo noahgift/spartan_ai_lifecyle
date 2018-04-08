6370a38de9c1a09e5d9ef5ba01a192d9981e2b23
Noah Gift <noah.gift@gmail.com>
2018-04-08 13:25:09 -0700

    Update aws_asyncio.py

--- aws_asyncio.py
+++ aws_asyncio.py
@@ -33,3 +33,31 @@ async def put_record(data,
         }
     )
     return response
+
+def gen_uuid_events():
+    """Creates a time stamped UUID based event"""
+
+    current_time = 'test-{date:%Y-%m-%d %H:%M:%S}'.format(date=datetime.datetime.now())
+    event_id = str(uuid.uuid4())
+    event = {event_id:current_time}
+    return json.dumps(event)
+
+def send_async_firehose_events(count=100):
+    """Async sends events to firehose"""
+
+    start = time.time() 
+    client = firehose_client()
+    extra_msg = {"aws_service": "firehose"}
+    loop = asyncio.get_event_loop()
+    tasks = []
+    LOG.info(f"sending aysnc events TOTAL {count}",extra=extra_msg)
+    num = 0
+    for _ in range(count):
+        tasks.append(asyncio.ensure_future(put_record(gen_uuid_events(), client)))
+        LOG.info(f"sending aysnc events: COUNT {num}/{count}")
+        num +=1
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
+    end = time.time()  
+    LOG.info("Total time: {}".format(end - start))
+
